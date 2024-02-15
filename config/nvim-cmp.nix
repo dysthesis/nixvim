dd848ba0e1c4e35d6776c1aff63d7d501e723239
Dysthesis <antheoraviel@protonmail.com>
2024-02-15 18:01:35 +1100

    Enable some extra completions

--- config/nvim-cmp.nix
+++ config/nvim-cmp.nix
@@ -54,6 +54,11 @@
     lspkind = {
       enable = true;
     };
+    cmp-nvim-lsp = {enable = true;}; # lsp
+    cmp-buffer = {enable = true;};
+    cmp-path = {enable = true;}; # file system paths
+    cmp_luasnip = {enable = true;}; # snippets
+    cmp-cmdline = {enable = true;}; # autocomplete for cmdline
   };
   extraConfigLua = ''
       kind_icons = {
