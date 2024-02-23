5e4b959fe1ea48c1db691ef343eb75208469e19f
Dysthesis <antheoraviel@protonmail.com>
2024-02-23 20:47:32 +1100

    Changes to obsidian nvim

--- config/obsidian.nix
+++ config/obsidian.nix
@@ -3,5 +3,28 @@
     enable = true;
     dir = "~/Documents/Episteme";
     completion.nvimCmp = true;
+    ui = {
+      enable = true;
+      updateDebounce = 100;
+      checkboxes = {
+        " " = {
+          char = "󰄱";
+          hlGroup = "ObsidianTodo";
+        };
+        "x" = {
+          char = "";
+          hlGroup = "ObsidianDone";
+        };
+        ">" = {
+          char = "";
+          hlGroup = "ObsidianRightArrow";
+        };
+        "~" = {
+          char = "󰰱";
+          hlGroup = "ObsidianTilde";
+        };
+      };
+      attachments.imgFolder = "~/Documents/Episteme/Resources/Images";
+    };
   };
 }
